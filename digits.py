a63e2cbf879528b2b15a8ef5ed532a6729451227
David Wu <dwu7401@gmail.com>
2015-01-29 18:42:28 -0800

    digits classification complete

--- digits.py
+++ digits.py
@@ -3,30 +3,34 @@ from sklearn.metrics import confusion_matrix
 import scipy.io
 import numpy as np
 import random
+import operator
 import matplotlib.pyplot as plt
 
 import time
 
 # Variables
 #Mac
-DIGIT_DIR="/Users/David/Documents/School/CS189/hw1/data/digit-dataset/"
+#DIGIT_DIR="/Users/David/Documents/School/CS189/hw1/data/digit-dataset/"
 #Linux
-#DIGIT_DIR="/home/david/Documents/cs189/hw1/data/digit-dataset/"
+DIGIT_DIR="/home/david/Documents/cs189/hw1/data/digit-dataset/"
 IMG_SIZE = 28*28 # known image size
 VALIDATION_SIZE = 10000
 N_SAMPLES = 60000
+TEST_SIZE = 10000
 
 # Load Training Set
 train = scipy.io.loadmat(DIGIT_DIR+"train")
 train_labels = train['train_labels']
 train_images = train['train_images'].transpose([2,0,1])
+test_images = scipy.io.loadmat(DIGIT_DIR+"test")['test_images'].transpose([2,0,1]).ravel().reshape((TEST_SIZE, IMG_SIZE))
 
 # Flatten Training Set
 flat = train_images.ravel().reshape((N_SAMPLES, IMG_SIZE))
 
 
-def trainValidateSVM(n, c=1.0):
-    clf = svm.SVC(kernel='linear')
+def trainValidateSVM(n, cv=1.0):
+    clf = svm.SVC(C=cv, kernel='linear')
+    print clf
     TRAIN_SIZE = n
 
     # Partition Training Set
@@ -39,31 +43,31 @@ def trainValidateSVM(n, c=1.0):
     X = np.array(X)
     y = np.array(y)
 
-    print "training on "+str(TRAIN_SIZE)+" samples"
+    #print "training on "+str(TRAIN_SIZE)+" samples"
     start = time.time()
     clf.fit(X, y)
     end = time.time()
-    print "done: "+str(end - start)
-    print "validating on "+str(VALIDATION_SIZE)+" samples"
+    #print "done: "+str(end - start)
+    #print "validating on "+str(VALIDATION_SIZE)+" samples"
     validation_indices = random.sample(xrange(len(flat)), VALIDATION_SIZE)
-    validate_x = []
-    validate_y = []
+    x_val = []
+    y_val = []
     for i in validation_indices:
-        validate_x.append(flat[i])
-        validate_y.append(train_labels[i][0])
-    predict = clf.predict(validate_x)
-    cm = confusion_matrix(validate_y, predict)
+        x_val.append(flat[i])
+        y_val.append(train_labels[i][0])
+    predict = clf.predict(x_val)
+    cm = confusion_matrix(y_val, predict)
 
     # Calculate accuracy
-    accuracy = clf.score(validate_x, validate_y)
-    print "done; printing confusion matrix:"
-    #print confusion_matrix
+    accuracy = clf.score(x_val, y_val)
+    #print "done; printing confusion matrix:"
+    # print confusion_matrix
     plt.matshow(cm)
     plt.title('Confusion matrix')
     plt.colorbar()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
-    plt.show()
+    plt.draw()
     return accuracy
 
 # C value for 'Penalty parameter C of the error term' in SVC
@@ -83,10 +87,10 @@ def tenFold(c=1.0):
     images = np.split(images, indices)
     labels = np.split(labels, indices)
     clf = svm.SVC(kernel='linear', C=c)
+    print clf
     values = []
-    for i in range(len(images)-1):
+    for i in range(len(images)):
         print "case "+str(i)
-        count = 0
         validation_images = images[i]
         validation_labels = labels[i]
         training_images = np.concatenate(images[0:i]+images[i+1:])
@@ -97,27 +101,62 @@ def tenFold(c=1.0):
         end = time.time()
         print "done: "+str(end - start)
         print "validation"
-        for i in range(len(validation_images)-1):
-            prediction = clf.predict(validation_images[i])
-            label = validation_labels[i]
-            if prediction == label:
-                count += 1
-        values.append(float(count) / len(validation_images))
+        #prediction = clf.predict(validation_images)
+        accuracy = clf.score(validation_images, validation_labels)
+        values.append(accuracy)
     print values
     average = sum(values) / len(values)
     return average
+
+def test(n, c=1.0):
+    clf = svm.SVC(kernel='linear', C=c)
+    TRAIN_SIZE = n
+    index = random.sample(xrange(len(flat)), TRAIN_SIZE)
+    X = []
+    y = []
+    for i in index:
+        X.append(flat[i])
+        y.append(train_labels[i][0])
+    X = np.array(X)
+    y = np.array(y)
+    clf.fit(X, y)
+    prediction = clf.predict(test_images)
+    print prediction
+    print len(prediction)
+    import csv
+    with open('results.csv', 'wb') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Id', 'Category'])
+        for i in range(len(prediction)):
+            writer.writerow([i+1, prediction[i]])
+
 
 
 #sample_sizes = [100, 200, 500, 1000, 5000, 10000]
 #for n in sample_sizes:
 #    print "Score for "+str(n)+": "+str(trainValidateSVM(n)*100)+"%"
-print str(trainValidateSVM(10000)*100)+"%"
 
-#print "Average 10-fold: "+str(tenFold()*100)+"%"
 #c_values = [ pow(10, x) for x in range(-5,6) ]
-#d = {}
+#for c in c_values:
+#    print "C value: "+str(c)+" Accuracy: "+str(trainValidateSVM(10000, c)*100)+"%"
+
+#print "Average 10-fold: "+str(tenFold(10)*100)+"%"
+
+#c_values = [ pow(10, x) for x in range(-5,6) ]
+#averages = []
 #for c in c_values:
 #    print "Testing C: "+str(c)
-#    d[c] = tenFold(c)
-#
-#print d
+#    averages.append(tenFold(c))
+#print c_values
+#print averages
+#index, value = max(enumerate(averages), key=operator.itemgetter(1))
+#print "Best C: "+str(c_values[index])
+#print "Best Average: "+str(value)
+
+#print str(trainValidateSVM(20000, 1e-05)*100)+"%"
+start = time.time()
+test(30000, 1e-05)
+end = time.time()
+print str(end - start)
+
+plt.show()
