fixed RSS calculation

hw4/hw4_2.py
@@ -25,17 +25,15 @@ beta = linalg.solve(X, Y)
 
 
 # Test
-#xTest = test_values.T * test_values
-#calc_values = xTest * beta
-#test_labels = test_values.T * test_labels
 calc_labels = test_values * beta
 RES = calc_labels - test_labels
-RSS = linalg.norm(RES.T * RES)
+# Matrix only has one value
+RSS = np.asarray(0.5 * (RES.T * RES))[0][0]
 
-print "RSS:"
-print RSS
 
-pdb.set_trace()
+print "RSS:" + str(RSS)
+
+#pdb.set_trace()
 
 plt.stem(np.asarray(beta).T[0])
 plt.show()