6788d1f344afdfb1579a546918b0bcb8a3b3b268
David Wu <dwu7401@gmail.com>
2015-03-12 19:04:28 -0700

    q3 done

--- hw4/hw4_3.py
+++ hw4/hw4_3.py
@@ -13,21 +13,17 @@ data = scipy.io.loadmat(DATA_DIR+"spam")
 xTrain = np.mat(data['Xtrain'])
 yTrain = np.mat(data['ytrain'])
 xTest = np.mat(data['Xtest'])
+BATCH_MAX_ITERATIONS=1000
+STO_MAX_ITERATIONS=5000
 
-# Preprocess Function: Takes 3 parameters; preprocess(matrix, matrixMEAN, matrixSTD)
-preprocess1 = np.vectorize(lambda val, mean, std: (val - mean) / std)
+preprocess1 = lambda mat: (mat - mat.mean(0)) / mat.std(0)
 preprocess2 = np.vectorize(lambda x: math.log(x + 0.1))
-preprocess3 = np.vectorize(lambda x: x > 0)
+preprocess3 = np.vectorize(lambda x: int(x > 0))
 
 # Preprocess data
 # Mean 0 and Unit Variance
-xTrainMean = xTrain.mean()
-xTrainSTD = np.std(xTrain)
-xTestMean = xTest.mean()
-xTestSTD = np.std(xTest)
-
-xTrain1 = preprocess1(xTrain, xTrainMean, xTrainSTD)
-xTest1 = preprocess1(xTest, xTestMean, xTestSTD)
+xTrain1 = preprocess1(xTrain)
+xTest1 = preprocess1(xTest)
 xTrain2 = preprocess2(xTrain)
 xTest2 = preprocess2(xTest)
 xTrain3 = preprocess3(xTrain)
@@ -38,19 +34,78 @@ def calculate_mu(X, beta):
     return mu
 
 
-def update_beta(X, Y, beta, L):
-    mu = calculate_mu(X, beta)
-    def diag(mu):
-        ones = np.ones((len(mu), 1))
-        retval = np.diagflat((ones - mu).T) * mu
-        return np.diagflat(retval)
-    A = diag(mu)
-    identity = np.identity(len(beta))
-    new_beta = beta - inv(2*L*identity - X.T * A * X) * (2*L*beta - X.T*(Y-mu))
+def update_beta(X, Y, beta, L, eta, stochastic=False):
+    if not stochastic:
+        mu = calculate_mu(X, beta)
+        new_beta = beta - eta * (2*L*beta - X.T * (Y - mu))
+    else:
+        i = np.random.randint(0, len(X))
+        Xi = X[i]
+        Yi = Y[i]
+        mu = calculate_mu(Xi, beta)
+        new_beta = beta - eta * (Yi - mu).T * Xi
     return new_beta
 
-def batch_gradient(X, Y, step_mode, lam, tolerance, max_iterations):
+def training_loss(X, Y, beta):
+    total_loss = 0
+    for i in range(len(X)):
+        mu = calculate_mu(X[i], beta)
+        total_loss += Y[i] * math.log(mu) + (1 - Y[i]) * math.log(1-mu)
+    return -1.0*float(total_loss)
+
+def batch_gradient(X, Y):
+    L = 1e-6
+    eta = 1e-5
     # Initialize beta to be zeros
     beta = np.mat(np.zeros(xTrain.shape[1])).T
+    change = float("inf")
+    losses = []
+    for i in range(BATCH_MAX_ITERATIONS):
+        prev = beta
+        beta = update_beta(X, Y, beta, L, eta)
+        change = np.linalg.norm(prev - beta)
+        if i % 100 == 0:
+            losses.append(training_loss(X, Y, beta))
+        if change < 1e-6:
+            break
+        #print losses[-1]
+    plt.plot(list(range(0, len(losses)*10, 10)), losses)
+    plt.show()
+    print "done"
+    return beta
+
+def stochastic_gradient(X, Y, variable=False):
+    L = 1e-6
+    eta = 1e-5
+    # Initialize beta to be zeros
+    beta = np.mat(np.zeros(xTrain.shape[1])).T
+    change = float("inf")
+    losses = []
+    for i in range(STO_MAX_ITERATIONS):
+        if variable:
+            eta = eta / float(i+1)
+        prev = beta
+        beta = update_beta(X, Y, beta, L, eta, stochastic=True)
+        change = np.linalg.norm(prev - beta)
+        if i % 100 == 0:
+            losses.append(training_loss(X, Y, beta))
+        if change < 1e-10:
+            print "done"
+            break
+        #print losses[-1]
+    print "done"
+    plt.plot(list(range(0, len(losses)*10, 10)), losses)
+    plt.show()
+    return beta
+
+batch_beta1 = batch_gradient(xTrain1, yTrain)
+#batch_beta2 = batch_gradient(xTrain2, yTrain)
+#batch_beta3 = batch_gradient(xTrain3, yTrain)
+#
+#sto_beta1 = stochastic_gradient(xTrain1, yTrain)
+#sto_beta2 = stochastic_gradient(xTrain2, yTrain)
+#sto_beta3 = stochastic_gradient(xTrain3, yTrain)
+#
+#varLearning_beta1 = stochastic_gradient(xTrain1, yTrain, variable=True)
 
 pdb.set_trace()
